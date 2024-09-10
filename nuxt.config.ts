3960e2ce15b1ebd69a0c3f4bba758848dc38e4d4
chan-mai <chan-mai@mq1.dev>
2024-09-10 20:25:30 +0900

    add: tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  ssr: true,
 
-ssr: true
+  css: [
+    '~/assets/css/tailwindcss.css'
+  ],
+  
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
