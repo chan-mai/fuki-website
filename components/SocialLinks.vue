ff9af6e1a1a291a624ba0ef42da4951aa043b283
chan-mai <chan-mai@mq1.dev>
2024-09-11 17:19:35 +0900

    feat: ソーシャルリンクを追加

---
+++ components/SocialLinks.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+    const socialAccounts = ref([
+     {
+        "name": "Bluesky",
+        "url": "https://bsky.app/profile/fuuuuki.bsky.social",
+        "icon": "simple-icons:bluesky",
+     },
+     {
+        "name": "Misskey",
+        "url": "https://misskey.flowers/@fuki",
+        "icon": "simple-icons:misskey",
+     },
+     {
+        "name": "Pixiv",
+        "url": "https://www.pixiv.net/users/32685096",
+        "icon": "simple-icons:pixiv",
+     },
+    ]);
+</script>
+
+<template>
+    <section class="my-4">
+        <ul class="flex justify-center gap-4">
+            
+            
+            <li v-for="account in socialAccounts" :key="account.name">
+                <a
+                    :href="account.url"
+                    target="_blank"
+                    rel="noopener"
+                    :aria-label="account.name"
+                    class="p-2 inline-block rounded-full border border-gray-300 text-gray-300 hover:text-gray-800 hover:border-gray-800 cursor-pointer transition-colors dark:border-gray-600 dark:text-gray-600 dark:hover:border-gray-300 dark:hover:text-gray-300"
+                >
+                <Icon :name="account.icon" class="w-[20px] h-[20px]" />
+            </a>
+            </li>
+            
+        </ul>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -73,6 +73,15 @@
         </section>
     <!-- Hero End-->
 
+    <!-- SocialLinks -->
+    <div class="mt-5 mb-10">
+        <h2 class="flex justify-center items-center my-12 mx-2 text-2xl text-center relative before:content-[''] after:content-[''] before:inline-block after:inline-block before:mx-4 after:mx-4 before:w-2 after:w-2 before:h-4 after:h-4 before:text-2xl after:text-2xl before:mb-2 after:mt-2 before:border-t before:border-l after:border-r after:border-b before:border-gray-400 after:border-gray-400">
+        SocialLinks
+        </h2>
+        <SocialLinks />
+    </div>
+    <!-- SocialLinks End -->
+
     <!-- Works -->
     <div class="mt-5 mb-10">
         <h2 class="flex justify-center items-center my-12 mx-2 text-2xl text-center relative before:content-[''] after:content-[''] before:inline-block after:inline-block before:mx-4 after:mx-4 before:w-2 after:w-2 before:h-4 after:h-4 before:text-2xl after:text-2xl before:mb-2 after:mt-2 before:border-t before:border-l after:border-r after:border-b before:border-gray-400 after:border-gray-400">
