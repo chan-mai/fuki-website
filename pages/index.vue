1857fb5d32b66c701458c6dda09e964a60b941e8
chan-mai <chan-mai@mq1.dev>
2024-09-11 17:27:11 +0900

    update: タイトル要素の共通化

--- pages/index.vue
+++ pages/index.vue
@@ -75,18 +75,14 @@
 
     <!-- SocialLinks -->
     <div class="mt-5 mb-10">
-        <h2 class="flex justify-center items-center my-12 mx-2 text-2xl text-center relative before:content-[''] after:content-[''] before:inline-block after:inline-block before:mx-4 after:mx-4 before:w-2 after:w-2 before:h-4 after:h-4 before:text-2xl after:text-2xl before:mb-2 after:mt-2 before:border-t before:border-l after:border-r after:border-b before:border-gray-400 after:border-gray-400">
-        SocialLinks
-        </h2>
+        <ContentTitle title="SocialLinks" />
         <SocialLinks />
     </div>
     <!-- SocialLinks End -->
 
     <!-- Works -->
     <div class="mt-5 mb-10">
-        <h2 class="flex justify-center items-center my-12 mx-2 text-2xl text-center relative before:content-[''] after:content-[''] before:inline-block after:inline-block before:mx-4 after:mx-4 before:w-2 after:w-2 before:h-4 after:h-4 before:text-2xl after:text-2xl before:mb-2 after:mt-2 before:border-t before:border-l after:border-r after:border-b before:border-gray-400 after:border-gray-400">
-        Works
-        </h2>
+        <ContentTitle title="Works" />
         <Works />
     </div>
     <!-- Works End -->
